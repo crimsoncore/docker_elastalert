d7b3d537bcad1c321210991aa1744758bdfeb284
luks_mbp <luks@crimsoncore.be>
2020-03-24 17:04:04 +0100

    added index creation for elastic in run.sh

--- run.sh
+++ run.sh
@@ -1,3 +1,4 @@
 #!/bin/sh
+python3 -m elastalert.create_index --index elastalert_status --config /opt/elastalert/config/config.yaml
 python3 -m elastalert.elastalert --config /opt/elastalert/config/config.yaml --rule /opt/elastalert/rules/netview.yaml --verbose --start $(date +"%Y-%m-%d")
 
